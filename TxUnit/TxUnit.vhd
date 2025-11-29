fc4bae84e4ba59f62151474e5e41ebc40e99a331
hocinemediani <dolcejeannarciss@gmail.com>
2025-11-29 12:52:46 +0100

    finished txunit

--- TxUnit/TxUnit.vhd
+++ TxUnit/TxUnit.vhd
@@ -2,7 +2,7 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity TxUnit is
-  port (
+  port(
     clk, reset : in std_logic;
     enable     : in std_logic;
     ld         : in std_logic;
@@ -14,14 +14,73 @@ end TxUnit;
 
 architecture behavorial of TxUnit is
 
-  type state is (IDLE, INIT, EMIT);
-  signal currentState : state;
+  -- Les differents etats de notre unite de transmission.
+  type states is (IDLE, INIT, EMIT);
+  -- L'etat actuel de notre unite de transmission.
+  signal state : states;
+  -- Un signal interne representatant le bufE out.
+  signal bufE_i : std_logic;
 
 begin
 
-  proc_name: process(clk, reset)
+  bufE <= bufE_i;
+
+  process(clk, reset)
+
+  -- Le buffer de donnees entrantes.
+  variable bufferT    : std_logic_vector(8 downto 0);
+  -- Le registre de donnees de transmission.
+  variable registerT  : std_logic_vector(8 downto 0);
+  --  Le compteur d'envoi de l'information.
+  variable compteur   : natural;
+
   begin
-    
+    if (reset = '0') then
+      txd       <= '1';
+      bufE_i    <= '1';
+      regE      <= '1';
+      compteur  := 8;
+      state     <= IDLE;
+    elsif (rising_edge(clk)) then
+      case state is
+        when IDLE =>
+          -- On initialise la transmission.
+          if (ld = '1') then
+            bufferT := '0' & data;
+            bufE_i  <= '0';
+            state   <= INIT;
+          end if;
+        when INIT =>
+          -- Etat intermediaire de recuperation/preparation des donnees.
+          registerT := bufferT;
+          bufE_i    <= '1';
+          regE      <= '0';
+          state     <= EMIT;
+        when EMIT =>
+          -- A chaque top de clk, on regarde si on recoit un autre ordre de
+          -- chargement de buffer de transmission.
+          if (ld = '1' and bufE_i = '1') then
+            bufferT := '0' & data;
+            bufE_i  <= '0';
+          end if;
+          -- Ordre de transmission.
+          if (enable = '1') then
+            if (compteur > 0) then
+              txd      <= registerT(compteur);
+            else
+              regE     <= '1';
+              txd      <= '1';
+              compteur := 8;
+              if (bufE_i = '1') then
+                state <= IDLE;
+              else
+                state <= INIT;
+              end if;
+            end if;
+            compteur := compteur - 1;
+          end if;
+        end case;
+    end if;
   end process;
 
 end behavorial;
--- clkUnit/clkUnit.vhd
+++ clkUnit/clkUnit.vhd
@@ -2,11 +2,10 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity clkUnit is
- port (
-        clk, reset : in  std_logic;
-        enableTX   : out std_logic;
-        enableRX   : out std_logic
-      );
+ port(
+    clk, reset : in  std_logic;
+    enableTX   : out std_logic;
+    enableRX   : out std_logic);
 end clkUnit;
 
 architecture behavorial of clkUnit is
