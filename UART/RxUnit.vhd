51e0c25212d3b197dd34ee44a3fe5d1a636bdf91
Aurelien VERBEKE <aurelien.verbeke@etu.toulouse-inp.fr>
2026-01-06 08:09:25 +0100

    rx: remove old process replaced by subcomponents

--- UART/RxUnit.vhd
+++ UART/RxUnit.vhd
@@ -61,44 +61,4 @@ begin
       DRdy => DRdy,
       data => data);
 
-
---  process(clk, reset)
---
---    type etats is (REPOS, COMPTAGE);
---    signal etat : etats;
---    variable compteur := 8;
---    variable compteurBit := 0;
---
---    begin
---      if (reset = '0') then
---        tmpClk <= '1';
---        etat <= IDLE; 
---      else if (rising_edge (clk)) then
---        case etat is
---          when REPOS =>
---            if (enable = '1') then
---              compteur := compteur - 1;
---              if (compteur = '0' and rxd = '0') then
---                etat <= COMPTAGE;
---                compteur := 8;
---                tmpClk <= 1;
---              end if;
---            end if;
---          when COMPTAGE =>
---            if (enable = '1') then
---              compteur := compteur - 1;
---              if (compteur = '0') then
---                tmpClk <= not tmpClk;
---                compteur := 8; 
---              end if;
---            end if;
---        end case;
---      end if;
---  end process;
---
---  process(clk, reset)
---    begin
---    
---  end process;
-
 end behavorial;
