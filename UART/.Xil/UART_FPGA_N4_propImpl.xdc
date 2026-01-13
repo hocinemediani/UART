ec8fcccfb6eac4e821a8e9d318bb1c1ca111346b
hocinemediani <dolcejeannarciss@gmail.com>
2026-01-13 14:47:52 +0100

    final push pdydyydidrdydydydydydy

--- UART/.Xil/UART_FPGA_N4_propImpl.xdc
+++ UART/.Xil/UART_FPGA_N4_propImpl.xdc
@@ -1,4 +1,4 @@
-set_property SRC_FILE_INFO {cfile:/home/ave9761/2a/projet_archi/UART/UART_FPGA_N4_DDR.xdc rfile:../UART_FPGA_N4_DDR.xdc id:1} [current_design]
+set_property SRC_FILE_INFO {cfile:/home/hmi2643/Annee_2/Architecture/UART/UART/UART_FPGA_N4_DDR.xdc rfile:../UART_FPGA_N4_DDR.xdc id:1} [current_design]
 set_property src_info {type:XDC file:1 line:10 export:INPUT save:INPUT read:READ} [current_design]
 set_property -dict { PACKAGE_PIN E3    IOSTANDARD LVCMOS33 } [get_ports { mclk }]; #IO_L12P_T1_MRCC_35 Sch=clk100mhz
 set_property src_info {type:XDC file:1 line:15 export:INPUT save:INPUT read:READ} [current_design]
--- UART/controleReception.vhd
+++ UART/controleReception.vhd
@@ -5,10 +5,10 @@ use IEEE.numeric_std.all;
 entity controleReception is
   port (
     clk 		: in std_logic;
-    reset 	    : in std_logic;
+    reset 	: in std_logic;
     read 		: in std_logic;
-    tmpClk 	    : in std_logic;
-    tmpRxd 	    : in std_logic;
+    tmpClk 	: in std_logic;
+    tmpRxd 	: in std_logic;
     FErr 		: out std_logic;
     OErr 		: out std_logic;
     DRdy 		: out std_logic;
@@ -17,9 +17,9 @@ end controleReception;
 
 architecture behavioral of controleReception is
 
-  signal buf : std_logic_vector(9 downto 0) := (others => 'U');
+  signal buf : std_logic_vector(8 downto 0) := (others => 'U');
 
-  type t_etat is (REPOS, RECEPTION, ATTENTE, FIN);
+  type t_etat is (REPOS, RECEPTION, ATTENTE);
   signal etat : t_etat := REPOS;
   
 begin
@@ -35,32 +35,28 @@ begin
         DRdy <= '0';
         FErr <= '0';
         OErr <= '0';
-        compteur := 9;
+        compteur := 8;
         compteur_read := 5;
-        data <= (others => 'U');
+        data <= (others => '0');
         etat <= REPOS;
-
       elsif (rising_edge(clk)) then
         case etat is
           when REPOS =>
-            if (tmpClk = '1' and tmpRxd = '0') then
+          if (tmpClk = '1' and tmpRxd = '0') then
+              data <= (others => '0');
               DRdy <= '0';
               FErr <= '0';
               OErr <= '0';
-              data <= (others => 'U');
               etat <= RECEPTION;
-              compteur := 9;
+              compteur := 8;
             end if;
-
           when RECEPTION =>
             -- donnée positionnée sur tmpRxd au front montant de tmpClk
             if (tmpClk = '1') then
               -- on la lit
               buf(compteur) <= tmpRxd;
-    
               if (compteur = 0) then
                 -- on est sur le bit de stop
-                
                 if (tmpRxd = '1') then
                   -- bit de stop correct
                   data <= buf(8 downto 1);
@@ -69,31 +65,28 @@ begin
                   FErr <= '0';
                   OErr <= '0';
                   etat <= ATTENTE;
-                
                 else
                   -- bit de stop incorrect
                   FErr <= '1';
-                  etat <= FIN;
+                  etat <= REPOS;
                 end if;
-              
               else
                 -- on est sur un autre bit : start ou donnée
                 compteur := compteur - 1;
               end if;
             end if;
-            
           when ATTENTE =>
             compteur_read := compteur_read - 1;
             if (compteur_read = 0) then
               OErr <= '1';
               DRdy <= '0';
+              etat <= REPOS;
+              buf <= (others => '0');
             elsif (read = '1') then
               DRdy <= '0';
+              etat <= REPOS;
+              buf <= (others => '0');
             end if;
-            etat <= FIN;
-          
-          when FIN =>
-            null;
         end case;
       end if;
     end process;
